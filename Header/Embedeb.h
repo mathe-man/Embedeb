2b8c6e069fdc893bc4b7b4378ddbc940be32d287
mathe-man <mathieumousset6@gmail.com>
2026-04-28 20:11:16 +0200

    Add the C++ header to the solution

---
+++ Header/Embedeb.h
@@ -0,0 +1,188 @@
+/*
+ * Copyright 2026 Mathieu Mousset
+ * Project: EmbeDeb (Embedded Debugger)
+ * Repository: https://github.com/mathe-man/EmbeDeb
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#pragma once
+#include <cstdint>
+#include <cstring>
+
+
+#define BoardName "Board"
+
+#define MessageSeparator "|"
+#define MessagesBufferSize 512
+
+#define EmbedDeb_MagicNumber "\xEB\xDB" // Magic number to identify EmbedDeb messages: 0xEBDB
+
+
+typedef uint16_t UnsignedInt; // This type can be changed depending of the needs
+
+class Event;
+
+struct EmbedDebMessage {
+    const char* sender;
+    const char* content;
+
+    EmbedDebMessage(const char* sender, const char* content) : sender(sender), content(content) {}
+
+    UnsignedInt inline Length() const {
+        return strlen(sender) + strlen(content) + strlen(MessageSeparator) + 1; // +1 for the "=" separator
+    }
+    const char* Build() const {
+        char* message = new char[strlen(sender) + strlen(content) + 2]; // +2 for "=" separator and null terminator
+
+        strcpy(message, sender);
+        strcat(message, "=");
+        strcat(message, content);
+        strcat(message, MessageSeparator);
+
+        return message;
+    }
+};
+
+typedef void (*WriteFunction)(const char*);
+
+class EmbedDeb {
+public:
+
+    static void setWriteFunction(WriteFunction func) {
+        writeFunction = func;
+    }
+
+    static inline bool Flush() {
+        return FlushBuffer();
+    }
+
+    static inline bool Log(EmbedDebMessage message)
+    {
+        return LogMessage(message);
+    }
+
+    static inline bool print(const char* value) {
+        if (!writeFunction)
+			return false; // No write function set, cannot print
+        
+        writeFunction(value);
+		return true;
+    }
+
+    static inline bool println(const char* value) {
+        if (!writeFunction)
+			return false; // No write function set, cannot print
+        
+        writeFunction(value);
+        writeFunction("\r\n");  // Finish the line with a newline and carriage return
+
+		return true;
+    }
+
+
+private:
+
+    // Function pointer for writing messages, use Serial.print by default
+    static inline WriteFunction writeFunction;
+
+    static inline char eventsMessagesBuffer[MessagesBufferSize] = ""; // Buffer to hold the messages before flushing
+
+    static inline bool FitInBuffer(EmbedDebMessage message) {
+        return strlen(eventsMessagesBuffer) + message.Length() + 1 < MessagesBufferSize; // +1 for null terminator
+    }
+
+    static inline UnsignedInt EmptyBufferSpace() {
+        return MessagesBufferSize - 1; // -1 for null terminator
+    }
+
+    static inline void ClearBuffer() {
+        eventsMessagesBuffer[0] = '\0';
+    }
+
+    static inline void AddToBuffer(EmbedDebMessage message) {
+        strcat(eventsMessagesBuffer, message.Build());   // The message build already include separator
+    }
+
+
+    // Event class can have access to logging
+    friend class Event;
+
+#define MaxLogAttempt 5
+
+    static inline bool LogMessage(EmbedDebMessage message, uint8_t attempt = 0) {
+        if (attempt >= MaxLogAttempt) {
+            return false;       // Max log attempts reached, give up
+        }
+        // Check if the message size is acceptable
+        if (message.Length() > EmptyBufferSpace())
+            return false;
+
+        // Check if the message can fit in the buffer
+        if (!FitInBuffer(message)) {
+            FlushBuffer();
+            return LogMessage(message, attempt++); // Try to log the message again after flushing the buffer
+        }
+
+        // All the test passed => Add the message to the buffer and return true
+        AddToBuffer(message);
+        return true;
+    }
+
+    static inline bool FlushBuffer()
+    {
+        if (strlen(eventsMessagesBuffer) == 0) {
+            return false; // Buffer is empty, no need to flush
+        }
+        // Send the serial communication with the format: MagicNumber|BoardName|message1|message2|...|messageN (Assuming the separator is '|')
+        print(EmbedDeb_MagicNumber); print(MessageSeparator);
+        print(BoardName); print(MessageSeparator);
+        println(eventsMessagesBuffer);
+        ClearBuffer();
+        return true;
+    }
+};
+
+
+
+class Event {
+public:
+    Event(const char* name) : eventName(name) {}
+
+    virtual const void flag() = 0;
+
+    const char* getName() const {
+        return eventName;
+    }
+
+
+protected:
+
+    bool inline Log(const char* content) {
+        EmbedDebMessage message(getName(), content);
+        return EmbedDeb::LogMessage(message);
+    }
+
+private:
+    const char* eventName;
+};
+
+class TickEvent : public Event {
+public:
+    TickEvent() : Event("TickEvent") {}
+
+    const void flag() override {
+        Log("Tick");
+    }
+};

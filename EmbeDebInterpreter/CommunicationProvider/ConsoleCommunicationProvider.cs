f2a703f3d5e867d8319616724b53ce8c1e72c472
mathe-man <mathieumousset6@gmail.com>
2026-04-28 22:41:44 +0200

    Add a communication provider using the console input

---
+++ EmbeDebInterpreter/CommunicationProvider/ConsoleCommunicationProvider.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace EmbeDebInterpreter.CommunicationProvider;
+
+public class ConsoleCommunicationProvider : ICommunicationProvider
+{
+    public event EventHandler<string>? OnCommunicationReceived;
+    public ConsoleCommunicationProvider()
+    {
+        Task.Run(() => ListenToConsoleInput());
+    }
+    private void ListenToConsoleInput()
+    {
+        while (true)
+        {
+            string input = Console.ReadLine() ?? string.Empty;
+            OnCommunicationReceived?.Invoke(this, input);
+        }
+    }
+}

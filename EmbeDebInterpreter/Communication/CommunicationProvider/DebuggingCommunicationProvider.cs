798857495624b1a70d6faa59cf5dff751ababac4
mathe-man <mathieumousset6@gmail.com>
2026-05-06 12:45:04 +0200

    Make a debug communication provider that can directly be used to test the code without a board

---
+++ EmbeDebInterpreter/Communication/CommunicationProvider/DebuggingCommunicationProvider.cs
@@ -0,0 +1,23 @@
+﻿
+namespace EmbeDebInterpreter.Communication.CommunicationProvider;
+
+public class DebuggingCommunicationProvider : ICommunicationProvider
+{
+    public event EventHandler<string>? OnCommunicationReceived;
+
+    public int defaultLatency;
+    public DebuggingCommunicationProvider(int defaultLatencyMs = 300)
+        => defaultLatency = defaultLatencyMs;
+
+    public void SendCommunication(string message, int latencyMs = -1)
+    {
+        if (latencyMs < 0)
+            latencyMs = defaultLatency;
+
+        Thread.Sleep(latencyMs);
+
+        OnCommunicationReceived?.Invoke(this, message);
+    }
+
+    // TODO: Create methods that take Message objects instead of strings then build the communication from this
+}

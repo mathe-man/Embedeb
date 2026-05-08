44841ba7826d9f3548e949d2fa1ad3b1f6a61d02
mathe-man <mathieumousset6@gmail.com>
2026-05-08 20:38:30 +0200

    Added new messages type handlers

---
+++ EmbeDebInterpreter/Message/MessageHandlers/BasicTimeMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿
+namespace EmbeDebInterpreter.Message.MessageHandlers;
+
+public class TimeMessage : Message
+{
+    public int Time { get; }
+    public TimeMessage(int time)
+        => Time = time;
+}
+
+[MessageHandler("TIME")]
+public class BasicTimeMessageHandler {
+    public static Message Handle(string content)
+    {
+        var time = int.Parse(content);
+        return new TimeMessage(time);
+    }
+}
---
+++ EmbeDebInterpreter/Message/MessageHandlers/BreakpointMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿
+namespace EmbeDebInterpreter.Message.MessageHandlers;
+
+public class BreakPointMessage : Message
+{
+    public DateTime ReceivedTime { get; }
+    public BreakPointMessage(DateTime receivedTime)
+        => ReceivedTime = receivedTime;
+}
+
+[MessageHandler("BKPoint")]
+public class BreakpointMessageHandler {
+    public static Message Handle(string content)
+    {
+        // You can add a breakpoint here for debugging purposes
+        return new BreakPointMessage(DateTime.Now);
+    }
+}

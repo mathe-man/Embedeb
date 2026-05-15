686a9407da69b0547ba0ed5db92e872386b84633
mathe-man <mathieumousset6@gmail.com>
2026-05-15 22:04:14 +0200

    Remove done tasks

--- EmbeDebInterpreter/Message/MessageHandler.cs
+++ EmbeDebInterpreter/Message/MessageHandler.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 
 namespace EmbeDebInterpreter.Message;
 
-// TODO: make this attribute support multiple message IDs for a single handler
-
-// TODO: make this attribute target method instead of class,
-//          then we can have multiple handlers in a single class.
-//          Also change the MessageDispatcher to support this new design.
 
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
 public class MessageHandler : Attribute

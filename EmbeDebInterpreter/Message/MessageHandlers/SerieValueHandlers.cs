d785740562976bf1f271bd22002e641987634487
mathe-man <mathieumousset6@gmail.com>
2026-05-15 21:33:28 +0200

    Handler: SerieValue, to receive a serie of linked value

---
+++ EmbeDebInterpreter/Message/MessageHandlers/SerieValueHandlers.cs
@@ -0,0 +1,23 @@
+﻿
+namespace EmbeDebInterpreter.Message.MessageHandlers;
+
+public class SerieValueMessage : Message
+{
+    public uint Index { get; }
+    public string Value { get; }
+    public SerieValueMessage(uint index, string value)
+    {
+        Index = index;
+        Value = value;
+    }
+}
+public class SerieValueHandlers
+{
+    [MessageHandler("SerieValue")]
+    public static Message Handle(RawMessage me)
+    {
+        SerieValueMessage result 
+            = new SerieValueMessage(uint.Parse(me.Content.Split(',')[0]), me.Content.Split(',')[1]);
+        return result;
+    }
+}
